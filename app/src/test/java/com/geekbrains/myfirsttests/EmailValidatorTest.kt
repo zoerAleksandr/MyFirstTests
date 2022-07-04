f8381ead5e438fa8bad39bc905f1a93147b98d53
zoer <zoer.aleksandr@gmail.com>
2022-07-05 00:58:43 +0300

    Добавил unit тесты

--- app/src/test/java/com/geekbrains/myfirsttests/EmailValidatorTest.kt
+++ app/src/test/java/com/geekbrains/myfirsttests/EmailValidatorTest.kt
@@ -7,6 +7,59 @@ import org.junit.Test
 class EmailValidatorTest {
 
     @Test
+    fun emailValidator_InvalidEmailWrongLangDomainV2_ReturnFalse() {
+        assertFalse(EmailValidator.isValidEmail("name@email.ком"))
+    }
+    @Test
+    fun emailValidator_InvalidEmailWrongLangDomainV1_ReturnFalse() {
+        assertFalse(EmailValidator.isValidEmail("name@домейн.com"))
+    }
+    @Test
+    fun emailValidator_InvalidEmailWrongLangName_ReturnFalse() {
+        assertFalse(EmailValidator.isValidEmail("имя@email.com"))
+    }
+    @Test
+    fun emailValidator_InvalidEmailSpaceBeforeEmail_ReturnFalse() {
+        assertFalse(EmailValidator.isValidEmail(" name@email.com"))
+    }
+    @Test
+    fun emailValidator_InvalidEmailSpaceInsideDomainV2_ReturnFalse() {
+        assertFalse(EmailValidator.isValidEmail("name@email.c om"))
+    }
+    @Test
+    fun emailValidator_InvalidEmailSpaceInsideDomainV1_ReturnFalse() {
+        assertFalse(EmailValidator.isValidEmail("name@ema il.com"))
+    }
+    @Test
+    fun emailValidator_InvalidEmailSpaceAfterAt_ReturnFalse() {
+        assertFalse(EmailValidator.isValidEmail("name@ email.com"))
+    }
+    @Test
+    fun emailValidator_InvalidEmailSpaceAfterLogin_ReturnFalse() {
+        assertFalse(EmailValidator.isValidEmail("name @email.com"))
+    }
+
+    @Test
+    fun emailValidator_InvalidEmailDoubleAt_ReturnFalse() {
+        assertFalse(EmailValidator.isValidEmail("name@@email.com"))
+    }
+
+    @Test
+    fun emailValidator_InvalidEmailNotAt_ReturnFalse() {
+        assertFalse(EmailValidator.isValidEmail("nameemail.com"))
+    }
+
+    @Test
+    fun emailValidator_InvalidEmailNotFullDomain_ReturnFalse() {
+        assertFalse(EmailValidator.isValidEmail("name@email."))
+    }
+
+    @Test
+    fun emailValidator_InvalidEmailAbsentDomain_ReturnFalse() {
+        assertFalse(EmailValidator.isValidEmail("name@.com"))
+    }
+
+    @Test
     fun emailValidator_CorrectEmailSimple_ReturnsTrue() {
         assertTrue(EmailValidator.isValidEmail("name@email.com"))
     }
